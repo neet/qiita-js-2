4cbc0a0745636e6638575ccef28c294ccb587b14
neet <n33t5hin@gmail.com>
2018-09-14 11:59:48 +0900

    エントリーポイントをrefine

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-import { Qiita } from './qiita';
+export * from './client/Gateway';
+export * from './client/Qiita';
 
-export default Qiita;
+export * from './entities/AccessToken';
+export * from './entities/AuthenticatedUser';
+export * from './entities/Comment';
+export * from './entities/ExpandedTemplate';
+export * from './entities/Group';
+export * from './entities/Item';
+export * from './entities/Like';
+export * from './entities/Project';
+export * from './entities/Reaction';
+export * from './entities/Tag';
+export * from './entities/Tagging';
+export * from './entities/Team';
+export * from './entities/TeamInvitation';
+export * from './entities/Template';
+export * from './entities/User';
+
+export * from './errors/QiitaError';
+export * from './errors/QiitaNotFoundError';
+export * from './errors/QiitaUnauthorizedError';
+export * from './errors/QiitaURLResolveError';
